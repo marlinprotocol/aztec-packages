cbfca5dfec99a29ccab45cb3e96cdd509da18362
sklppy88 <esau@aztecprotocol.com>
2024-08-22 12:57:08 +0000

    init

--- yarn-project/end-to-end/src/e2e_fees/private_refunds.test.ts
+++ yarn-project/end-to-end/src/e2e_fees/private_refunds.test.ts
@@ -101,7 +101,7 @@ describe('e2e_fees/private_refunds', () => {
         t.aliceAddress,
         token.address,
         deriveStorageSlotInMap(TokenContract.storage.balances.slot, t.aliceAddress),
-        TokenContract.notes.TokenNote.id,
+        TokenContract.notes.IntNote.id,
         txHash,
       ),
     );
@@ -122,7 +122,7 @@ describe('e2e_fees/private_refunds', () => {
         t.bobAddress,
         token.address,
         deriveStorageSlotInMap(TokenContract.storage.balances.slot, t.bobAddress),
-        TokenContract.notes.TokenNote.id,
+        TokenContract.notes.IntNote.id,
         txHash,
       ),
     );
